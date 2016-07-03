88e629c4ba69b91940ea592af875ab7c8a13e1e7
prayas <kabirsahni8@gmail.com>
2016-07-03 20:35:39 +0000

    firstcommit

--- server.js
+++ server.js
@@ -8,35 +8,23 @@ var passport = require('passport');
 var session = require('express-session');
 var app = express();
 app.use(bp.urlencoded({ extended:true}));
-app.use(express.static(path.join(__dirname, 'public')));
-app.get(/^\/(.+)/,function(req,resp){
-    //console.log(req.body);
-	//console.log('hii');
-	//console.log(req.params[0]);
-	var obj={};
-	var temp=req.params[0];
-	if(/^[0-9]{1,}/.test(temp)){
-	
-		var y=new Date(req.params[0]/1000).toString();
-		if(y==='Invalid Date')
-        {obj.natural=null;
-        obj.unix=null;}
-        else{
-		obj.unix=req.params[0]
-		obj.natural=y.substr(4,11);
-        }
-	}
-	else{
-		obj.unix=Date.parse(req.params[0]);
-		if(isNaN(obj.unix)==false)
-		{obj.natural=req.params[0];}
-		else{obj.natural=null;}
-		console.log(obj);
-	}
-		
-resp.send(obj);
-  });
-var port = process.env.PORT || 8080;
+
+//app.use(express.static(path.join(__dirname, 'public')));
+app.get('/',function(req,resp){
+    var x=req.headers['x-forwarded-for'];
+    var z=req.headers['accept-language'];
+    var y=req.headers['user-agent'];
+    var match1=/^(.*);/.exec(z);
+    var match=/.*?(\(.*?\)).*/.exec(y);//return the array with first element as matched string and other the captured object(note:? for non-greedy matching)
+    var obj={
+        ipaddress:x,
+        language:match1[0],
+        operatingsystem:match[1]
+    };
+    resp.send(JSON.stringify(obj,null,40));
+
+});
+var port =8080;
 app.listen(port,  function () {
 	console.log('Node.js listening on port ' + port + '...');
 });
