9c5c1b9695ca7cc90741cb33b87596dc420ebfbd
prayas <kabirsahni8@gmail.com>
2016-07-03 14:13:51 +0000

    secondcomit

--- server.js
+++ server.js
@@ -1,18 +1,22 @@
 
 var express = require('express');
+var path=require("path");
 var routes = require('./app/routes/index.js');
 var mongoose = require('mongoose');
+var bp=require('body-parser');
 var passport = require('passport');
 var session = require('express-session');
 var app = express();
-app.use(express.static(process.cwd+'/public/index.html'));
-//app.use('/public', express.static(process.cwd() + '/public'));
+app.use(bp.urlencoded({ extended:true}));
+app.use(express.static(path.join(__dirname, 'public')));
 app.get(/^\/(.+)/,function(req,resp){
+    //console.log(req.body);
 	//console.log('hii');
 	//console.log(req.params[0]);
 	var obj={};
 	var temp=req.params[0];
 	if(/^[0-9]{1,}/.test(temp)){
+	
 		var y=new Date(req.params[0]/1000).toString();
 		if(y==='Invalid Date')
         {obj.natural=null;
@@ -29,6 +33,7 @@ app.get(/^\/(.+)/,function(req,resp){
 		else{obj.natural=null;}
 		console.log(obj);
 	}
+		
 resp.send(obj);
   });
 var port = process.env.PORT || 8080;
